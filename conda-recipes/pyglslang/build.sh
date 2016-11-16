54cc135c68870729486163f914ae521269359b1c
Mathieu Lamarre <mathieu@vlam3d.com>
2016-11-15 22:47:07 -0500

    conda build scripts for linux

---
+++ conda-recipes/pyglslang/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir build
+cd build
+
+cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DNUMPY_SWIG_DIR=/home/mathieu/dev/pyvulkan/numpy_swig -DCMAKE_BUILD_TYPE=Release -DVULKAN_SDK=/home/mathieu/vulkan/VulkanSDK/1.0.30.0
+
+cmake --build . --config Release --target _pyglslang
+
+cp "$SRC_DIR/build/_pyglslang.so" "$SP_DIR"
+cp "$SRC_DIR/build/pyglslang.py" "$SP_DIR"
